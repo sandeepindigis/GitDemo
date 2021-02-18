60e384e2630b7cf19fe9eaf20ce90aae48303826
sandeep3587 <sandeep3587@gmail.com>
2021-02-19 00:19:22 +0530

    add new feature

---
+++ app/src/main/java/com/example/gitdemo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.gitdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv=findViewById(R.id.tv_name);
+    }
+}
